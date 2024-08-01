1e155d695a27e27110384b1982a122de2d6c8fdb
rei <67372053+reiugit@users.noreply.github.com>
2024-08-01 19:44:41 +0200

    Updated example titles.

--- OneOfExample/Program.cs
+++ OneOfExample/Program.cs
@@ -6,32 +6,32 @@ var oneOfErrorResult = Divider
     .PrintNumbers()
     .Divide();
 
-// 1. Example for  OneOf.Match
+// 1. Example for:  OneOf.Match
 oneOfErrorResult
     .Match(
         value => value.ToString(),
         error => error.Value)
     .Print("Using 'OneOf.Match'");
 
-// 2. Example for  OneOf.Switch
+// 2. Example for:  OneOf.Switch
 oneOfErrorResult
     .Switch(
         value => value.Print("Using 'OneOf.Switch'"),
         error => error.Value.Print("Using 'OneOf.Switch'"));
 
-// 3. Example for  OneOf.TryPickT0
+// 3. Example for:  OneOf.TryPickTx
 (oneOfErrorResult
     .TryPickT0(out var value, out var error)
         ? value.ToString()
         : error.Value)
-    .Print("Using 'OneOf.TryPickT0'");
+    .Print("Using 'OneOf.TryPickTx'");
 
-// 4. Example for  OneOf.IsT0 and AsTo, AsT1
+// 4. Example for:  OneOf.IsTx and AsTx
 (oneOfErrorResult
     .IsT0
         ? oneOfErrorResult.AsT0.ToString()
         : oneOfErrorResult.AsT1.Value)
-    .Print("Using 'OneOf.IsT0 and AsTo, AsT1'");
+    .Print("Using 'OneOf.IsTx and OneOf.AsTx'");
 
 
 Console.WriteLine("\n\nPress any key to exit...");
